e01bfa71a2052b4c4fa518bf255ff96b7a685c05
mohammedtakbir <takbirhossain678@gmail.com>
2023-02-27 09:22:47 +0600

    add see more button functionality

--- practice/js/phone.js
+++ practice/js/phone.js
@@ -1,15 +1,25 @@
-async function loadPhones(search) {
+async function loadPhones(search, dataLimit) {
+    loading(true);
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${search}`);
     const data = await res.json();
-    displayPhones(data.data);
+    displayPhones(data.data, dataLimit);
 };
-function displayPhones(phones) {
-    const error = document.getElementById('error')
+function displayPhones(phones, dataLimit) {
+    const error = document.getElementById('error');
+    const seeMore = document.getElementById('see-more-btn');
     if (phones.length < 1) {
         loading(false);
         error.classList.remove('d-none');
+        seeMore.classList.add('d-none');
     } else {
         error.classList.add('d-none');
+    }
+
+    if (dataLimit && phones.length > 10) {
+        phones = phones.slice(0, 10);
+        seeMore.classList.remove('d-none');
+    }else{
+        seeMore.classList.add('d-none');
     }
 
     const phoneContainer = document.getElementById('phone-container');
@@ -36,11 +46,25 @@ function displayPhones(phones) {
     }
 };
 
-document.getElementById('search-btn').addEventListener('click', function () {
+function dataProcess(dataLimit) {
     loading(true);
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    loadPhones(searchText);
+    loadPhones(searchText, dataLimit);
+}
+
+document.getElementById('search-btn').addEventListener('click', function () {
+    dataProcess(10);
+})
+
+document.getElementById('search-field').addEventListener('keypress', (e) => {
+    if(e.key === 'Enter'){
+        dataProcess(10);
+    }
+})
+
+document.getElementById('see-more-btn').addEventListener('click', () => {
+    dataProcess();
 })
 
 function loading(isLoading) {
